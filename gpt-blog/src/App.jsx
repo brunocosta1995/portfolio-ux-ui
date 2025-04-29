5ceb9bc0c9c0265d7fb39bda84c7da48eb8fb9a9
Bruno Costa <costabruno.dev@gmail.com>
2025-04-28 21:40:00 -0300

    first commit

---
+++ gpt-blog/src/App.jsx
@@ -0,0 +1,30 @@
+import {
+  Footer,
+  Blog,
+  Possibility,
+  Features,
+  WhatGPT3,
+  Header,
+} from "./containers";
+
+import { CTA, Brand, Navbar } from "./components";
+
+import './App.css';
+
+export default function App() {
+  return (
+    <div className="App">
+      <div className="gradient-bg">
+        <Navbar />
+        <Header />
+      </div>
+      <Brand />
+      <WhatGPT3 />
+      <Features />
+      <Possibility />
+      <CTA />
+      <Blog />
+      <Footer />
+    </div>
+  );
+}
